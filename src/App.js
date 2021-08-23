742dfb7469cc128be85d2352259db985d1edaa11
ohmpatel46 <ohmpatel46@gmail.com>
2021-08-23 10:05:53 +0530

    V 0.0.1
    Initial commit after setting up the react app.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Lets build the mern-tinder-clone</h1>
+    </div>
+  );
+}
+
+export default App;
