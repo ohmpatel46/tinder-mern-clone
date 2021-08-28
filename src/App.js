dd778a004d3d595a1cd989b8f9803f359988f970
ohmpatel46 <ohmpatel46@gmail.com>
2021-08-28 10:28:50 +0530

    V 0.0.4
    Frontend almost complete.
    Ready to add backend.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/Header';
 import TinderCards from './components/TinderCards';
+import SwipeButtons from './components/SwipeButtons';
 function App() {
   return (
     <div className="app">
