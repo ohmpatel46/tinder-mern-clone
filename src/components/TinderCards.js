5ce09da152a9be21705c392df341db5f8a5b30d7
ohmpatel46 <ohmpatel46@gmail.com>
2021-08-31 14:06:00 +0530

    V 1.0.0
    Hooked up the frontend with the API.
    
    MVP ready.

--- src/components/TinderCards.js
+++ src/components/TinderCards.js
@@ -1,28 +1,28 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 import './TinderCards.css';
 import TinderCard from 'react-tinder-card';
+import Axios from './axios.js';
 
 function TinderCards(){
-    const [people,setPeople]= useState([
-        {
-            name:"Félix Lengyel",
-            url:"https://pbs.twimg.com/profile_images/1188911868863221772/fpcyKuIW_400x400.jpg"
-        },
-        {
-            name:"Janne Stefanovski",
-            url:"https://www.vpesports.com/wp-content/uploads/2020/08/gorgc-OG1.jpg"
-        },
-        {
-            name:"Jaron",
-            url:"https://estnn.com/wp-content/uploads/2020/11/monkeys-forever-dota.jpg"
+    const [people,setPeople]= useState([]);
+
+    useEffect(()=>{
+        async function fetchData(){
+            const req= await Axios.get("/tinder/cards");
+            setPeople(req.data);
         }
-    ]);
+        fetchData();
+    });
+
+
     const swiped=(dir,name)=>{
         console.log(`Removing ${name}`);
     };
+
     const outOfFrame= (name)=>{
         console.log(`${name} is out of the frame`);
     };
+    
     return(
         <div className="TinderCards">
             <div className="TinderCards__CardContainer">
--- tinder_backend/server.js
+++ tinder_backend/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import Cards from './dbCards.js';
+import Cors from 'cors';
 
 //App Config
 
@@ -8,6 +10,9 @@ const port= process.env.PORT || 8001;
 const connection_url=`mongodb+srv://Admin:t2bZJLARNdBUUBy@cluster0.fc7jj.mongodb.net/tinderdb?retryWrites=true&w=majority`;
 
 //Middleware
+
+app.use(express.json());
+app.use(Cors());
 
 //DB Config
 
@@ -23,6 +28,30 @@ app.get('/', (req,res)=>{
         res.status(200).send("Hello World")
     );
 });
+
+app.post('/tinder/cards', (req,res)=>{
+    const dbCards= req.body;
+    Cards.create(dbCards,(err,data)=>{
+        if(err){
+            res.status(500).send(err);
+        }
+        else{
+            res.status(201).send(data);
+        }
+    });
+});
+
+app.get('/tinder/cards', (req,res)=>{
+    Cards.find((err,data)=>{
+        if(err){
+            res.status(500).send(err);
+        }
+        else{
+            res.status(200).send(data);
+        }
+    });
+});
+
 
 //Listener
 app.listen(port, ()=>{
